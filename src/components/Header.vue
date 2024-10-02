d6a27055d2e6c8306aec68520fd90016f01bcdf6
serg473 <sergeidsi473@gmail.com>
2024-10-03 00:18:43 +0300

    feat: install tailwind and create mobile design header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script></script>
+<template>
+    <div class="container min-[360px]:max-w-[928px] min-[1024px]:max-w-[1120px]">
+            <header>
+                <div class="flex justify-between p-[13px_0px_11px_0px]">
+                    <img class="max-[360px]:w-[121px] max-[360px]:h-[44px]" src="../assets/img/logo.svg" alt="Logo">
+                    <button>
+                        <img src="../assets/img/burger.svg" alt="Burger">
+                    </button>
+                </div>
+            </header>
+    </div>
+</template>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,13 @@
 </script>
 
 <template>
-  <div>
-    Main content
+  <div class="bg-[url('../assets/img/travel-mob.png')] bg-no-repeat bg-cover p-[56px_0px_80px_0px]">
+    <div
+      class="container mb-11 flex flex-col items-center min-[360px]:max-w-[928px] min-[1024px]:max-w-[1120px] relative">
+      <h1>Путешествия мечты</h1>
+      <h2>Выбери свою</h2>
+      <img width="16" height="51" src="../assets/img/title-line.svg" alt="Line Title">
+      <div />
+    </div>
   </div>
 </template>
